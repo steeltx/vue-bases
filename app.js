63afb44360f1aae2f40b8dc927bc482a32feb6af
steeltx <realsteel.gnu@gmail.com>
2021-08-04 21:30:17 -0500

    Estado con Data

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const app = Vue.createApp({
-    template: `
-    <h1>Hola mundo</h1>
-    <p>Desde app.js</p>
-    `
+    // template: `
+    // <h1>Hola mundo</h1>
+    // <p>Desde app.js</p>
+    // `
+    data(){
+        return {
+            quote: "I'm Bat",
+            author: 'Bruce Wayne'
+        }
+    }
 })
 
 app.mount('#myApp')
