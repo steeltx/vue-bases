23f86f6c63f7bec900c66719b51c0f5eae6a69f3
steeltx <realsteel.gnu@gmail.com>
2021-08-04 22:11:47 -0500

    Intro  a eventos

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const app = Vue.createApp({
     // <h1>Hola mundo</h1>
     // <p>Desde app.js</p>
     // `
-    data(){
+    data() {
         return {
             quote: "I'm Bat",
             author: 'Bruce Wayne'
+        }
+    },
+    methods: {
+        changeQuote(event) {
+            console.log('hola',event)
+            this.author = 'Fernando'
+            this.capitalize()
+        },
+        capitalize(){
+            this.quote = this.quote.toUpperCase()
         }
     }
 })
